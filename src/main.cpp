40a833ca0d949e9a47d08113ddf43ce2c922a95e
meda000 <pavle.meda00@gmail.com>
2023-03-30 20:34:20 +0200

    blending i face cull

--- src/main.cpp
+++ src/main.cpp
@@ -163,7 +163,13 @@ int main() {
     // -----------------------------
     glEnable(GL_DEPTH_TEST);
 
+    // face culling
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
 
+    // blending
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
     // build and compile shaders
     // -------------------------
